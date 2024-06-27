76a2e2746b04f31907487439d53cd640786dddf8
toxicf1sh <disc4473@gmail.com>
2024-06-27 18:52:00 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+const yes = document.querySelector("#yes");
+      const no = document.querySelector("#no");
+      const confirmation = document.querySelector(".confirmation");
+      const container = document.querySelector(".container");
+
+      let clickCounter = 0;
+
+      const yesAnswerMethod = () => {
+        container.style.display = 'none';
+        confirmation.style.display = 'block';
+      };
+
+      const noAnswerMethod = () => {
+        no.style.position = 'absolute';
+
+        let xPosition = Math.floor(Math.random() * container.clientWidth);
+        let yPosition = Math.floor(Math.random() * container.clientHeight);
+
+        no.style.top = `${yPosition}px`;
+        no.style.left = `${xPosition}px`;
+
+        clickCounter++;
+
+        if (clickCounter > 4) {
+          window.open('https://papertoilet.com', '_blank');
+        }
+      };
+
+      no.addEventListener('click', noAnswerMethod);
+      yes.addEventListener('click', yesAnswerMethod);
+
+      document.getElementById("fix").addEventListener("click", function() {
+        window.location = "https://www.instagram.com/toxicf1sh";
+      }, false);
